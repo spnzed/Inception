00ae490a4d39edcd2b3d0c8ca7325ca52a5daf0c
Aaron Espinosa <aaespino@student.42barcelona.com>
2025-02-03 12:14:14 +0100

    mariadb do

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#! /bin/bash
 
-# Iniciar MariaDB en primer plano
-mysqld_safe --datadir='/var/lib/mysql' &
-PID=$!
+# Start mysql
+mysqld_safe &
 
-# Esperar a que MariaDB esté disponible
-while ! mysqladmin ping -h localhost --silent; do
-    sleep 1
-done
+sleep 10;
 
-# Crear base de datos y usuario si no existen
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# Root conf
+# Create the user if it doesn't exist
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# Grant all privileges to the root user
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
+# Refresh all so that MySQL takes it into account
 mysql -e "FLUSH PRIVILEGES;"
 
-# Dejar que el proceso de MariaDB continúe ejecutándose en primer plano
-wait $PID
+
+# Create a DB if does not exist. Named after the variable MYSQL_DATABASE in .env file
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+# Give rights to the user
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# Refresh all so that MySQL takes it into account
+mysql -e "FLUSH PRIVILEGES;"
+
+# Keep the MySQL process running in the foreground
+wait
