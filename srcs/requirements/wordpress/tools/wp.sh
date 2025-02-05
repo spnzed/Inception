f219ee1ffd4ecabc92b5339a5b96adfd2cc01676
Aaron Espinosa <aaespino@student.42barcelona.com>
2025-02-05 13:32:00 +0100

    wp.sh typo and restruct ._.

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,17 +1,42 @@
-#! /bin/bash
+#!/bin/bash
 
-# This script is used to install wordpress and create a user
-# The script will check if wordpress is already installed or not
-# If not installed, it will download wordpress, create a config file, install wordpress and create a user
-if [ -f ./wp-config.php ]
-then
-	echo "Wordpress already exists"
+if [ -f ./wp-config.php ]; then
+  echo "WordPress already exists"
 else
-	wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
-	wp core install --url=$DOMAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIM --admin_password=$WORDPRESS_ADMIM_PASS  --admin_email=$WORDPRESS_ADMIM_EMAIL --skip-email --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
-	wp theme install astra --activate --allow-root
+  wp core download --allow-root
+  wp config create \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=$MYSQL_HOSTNAME \
+    --allow-root
+
+  # Wait for MySQL
+  until mysqladmin ping -h "$MYSQL_HOSTNAME" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
+    echo "Waiting for MySQL..."
+    sleep 2
+  done
+
+  # Install WordPress
+  wp core install \
+    --url=$DOMAIN_NAME \
+    --title="$WORDPRESS_TITLE" \
+    --admin_user=$WORDPRESS_ADMIN \
+    --admin_password=$WORDPRESS_ADMIN_PASS \
+    --admin_email=$WORDPRESS_ADMIN_EMAIL \
+    --skip-email \
+    --allow-root
+
+  # Create additional user
+  wp user create \
+    $WORDPRESS_USER \
+    $WORDPRESS_EMAIL \
+    --role=author \
+    --user_pass=$WORDPRESS_USER_PASS \
+    --allow-root
+
+  # Install theme
+  wp theme install astra --activate --allow-root
 fi
 
 exec php-fpm7.4 -F
